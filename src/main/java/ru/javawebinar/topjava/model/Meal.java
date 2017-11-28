42d1d299fdd5b0f848a787b9799bfc0197adead5
mortinelli <seer_os@mail.ru>
2017-11-28 18:25:19 +0200

    HW4 not all

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,45 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m left join FETCH m.user WHERE m.user.id =:userId ORDER BY date_time DESC "),
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m  WHERE m.user.id=:userId AND m.id=:id "),
+        @NamedQuery(name = Meal.GET, query = "SELECT m FROM Meal m left join FETCH m.user WHERE m.user.id=:userId AND m.id=:id")
+})
+
+
+
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = "date_time", name = "date_time")})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String DELETE = "Meal.delete";
+    public static final String GET = "Meal.get";
+
+
+    @Column(name = "date_time", nullable = false, unique = true)
+    @NotBlank
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotNull
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @NotNull
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn( name="user_id")
     private User user;
 
     public Meal() {
--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
@@ -1,33 +1,63 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional (readOnly = true)
 public class JpaMealRepositoryImpl implements MealRepository {
 
+
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        return null;
+        User ref = em.getReference(User.class, userId);
+        meal.setUser(ref);
+            if (meal.isNew()) {
+                em.persist(meal);
+                return meal;
+            } else {
+                return em.merge(meal);
+            }
+
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createNamedQuery(Meal.DELETE)
+                .setParameter("id",id)
+                .setParameter("userId",userId)
+                .executeUpdate() !=0;
     }
 
     @Override
+    @Transactional
     public Meal get(int id, int userId) {
-        return null;
+        return em.createNamedQuery(Meal.GET,Meal.class)
+                .setParameter("id", id)
+                .setParameter("userId", userId).getSingleResult();
     }
 
+  //@NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m left join FETCH m.user WHERE m.user.id =:userID ")
+
     @Override
+    @Transactional
     public List<Meal> getAll(int userId) {
-        return null;
+        return em.createNamedQuery(Meal.ALL_SORTED,Meal.class)
+                .setParameter("userId",userId)
+                .getResultList();
     }
 
     @Override
